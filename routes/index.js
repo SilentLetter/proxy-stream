ef99857bdfe5a2e30e0419ff7943ff49c63e8d39
SilentLetter <67179446+SilentLetter@users.noreply.github.com>
2020-06-27 21:50:22 +0300

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -2,9 +2,8 @@
 
 const express = require('express')
 const router = express.Router()
-
-router.get('/', (req, res) => res.end('[Free] Proxy Stream Drive'));
+//router.get('/', (req, res) => res.end('[Free] Proxy Stream Drive'));
 router.get('/sources', require('../controllers/get_video_infos'));
-router.get('/videoplayback', require('../controllers/videoplayback'));
+router.get('/ozlem', require('../controllers/ozlem'));
 
 module.exports = router
